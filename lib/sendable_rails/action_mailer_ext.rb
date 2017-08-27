43d4c4943dc81e276443eb585ce11d112e7d12bb
Umair Siddique <umairsiddique@gmail.com>
2017-08-27 09:02:44 +0500

    Retrieve assigns from instance variables.

--- lib/sendable_rails/action_mailer_ext.rb
+++ lib/sendable_rails/action_mailer_ext.rb
@@ -9,7 +9,16 @@ module SendableRails
         api_key = SendableRails.config.api_key
 
         uri = URI("https://api.sendable.io/v1/project/#{project_id}/template/#{sendable[:template_id]}/render")
+
+        assigns = {}
+        instance_variables.each do |key|
+          if match = key.to_s.match(/@([^\_]+)/)
+            assigns[match[1]] = instance_variable_get(match[0])
+          end
+        end
+
         params = {
+          assigns: assigns,
         }
 
         res = Net::HTTP.post_form(uri, params)
